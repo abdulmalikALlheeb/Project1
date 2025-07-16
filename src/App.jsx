32bcd7973ef5c902adb6d54ff2f237c7f145f75f
abdulmalikALlheeb <abdumalik.m4464@gmail.com>
2025-07-16 03:06:07 +0300

    add 6 line 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
-
   return (
     <div>
       <div>jjjjjj</div>
       lllll
+      <h1>hhhh</h1>
+      <h1>hhhh</h1>
+      <h1>hhhh</h1>
+      <h1>hhhh</h1>
+      <h1>hhhh</h1>
       <h1>hhhh</h1>
     </div>
   );
